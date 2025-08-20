26ad268c22d35f0a859cd7ea81bb24ec90e319d0
Hemnaath-B <hemnaath_balasubramani@epam.com>
2025-08-20 20:12:26 +0530

    Create src/test/java/stepDefinitions/LoginFunctionalitySteps.java

---
+++ src/test/java/stepDefinitions/LoginFunctionalitySteps.java
@@ -0,0 +1,43 @@
+// NOTE: Update XPath and ID selectors as needed for your target website.
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.assertTrue;
+
+public class LoginFunctionalitySteps {
+
+    WebDriver driver;
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://example.com/login");
+    }
+
+    @When("the user enters valid credentials")
+    public void the_user_enters_valid_credentials() {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        usernameField.sendKeys("validUsername");
+        passwordField.sendKeys("validPassword");
+    }
+
+    @When("the user clicks the login button")
+    public void the_user_clicks_the_login_button() {
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        loginButton.click();
+    }
+
+    @Then("the user should be redirected to the homepage")
+    public void the_user_should_be_redirected_to_the_homepage() {
+        assertTrue(driver.getCurrentUrl().contains("homepage"));
+        driver.quit();
+    }
+}
