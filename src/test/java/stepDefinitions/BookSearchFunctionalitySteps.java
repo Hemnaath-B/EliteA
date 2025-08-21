eab730a5f55aea430b350ee747c76407d05b8dcb
Hemnaath-B <hemnaath_balasubramani@epam.com>
2025-08-21 14:56:36 +0530

    Create src/test/java/stepDefinitions/BookSearchFunctionalitySteps.java

---
+++ src/test/java/stepDefinitions/BookSearchFunctionalitySteps.java
@@ -0,0 +1,10 @@
+// NOTE: Update XPath and ID selectors as needed for your target website.
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
